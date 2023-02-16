16cbbec8621d863a7608258bfe824d3ed48926ff
Lakomka2204 <deniskozachok22@gmail.com>
2023-02-16 13:39:33 +0200

    missing libs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace QR_Reader_Desktop
@@ -13,7 +16,24 @@ namespace QR_Reader_Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
+            List<string> RequiredFiles = new List<string>()
+                {
+                "zxing.dll",
+                "zxing.presentation.dll",
+                "AForge.dll",
+                "AForge.Video.DirectShow.dll",
+                "AForge.Video.dll",
+                "QRCoder.dll"
+            };
+            RequiredFiles.RemoveAll(File.Exists);
+            if (RequiredFiles.Any())
+            {
+                MessageBox.Show($"Missing files\r\n{string.Join("\r\n",RequiredFiles)}",Settings.AppName,MessageBoxButtons.OK,MessageBoxIcon.Error);
+                Environment.Exit(-1);
+                return;
+            }
+            RequiredFiles = null;
+            GC.Collect();
             Application.Run(new Main());
         }
     }
